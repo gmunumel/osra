9429c567c317673cb0da47cdef37056771bd8f7b
Christopher Milne <christopher.milne@gmail.com>
2015-02-20 13:27:05 -0500

    merge

--- app/controllers/hq/orphans_controller.rb
+++ app/controllers/hq/orphans_controller.rb
@@ -1,2 +1,20 @@
 class Hq::OrphansController < HqController
+  def index
+    @orphans = Orphan.paginate(:page => params[:page])
+  end
+
+  # def new
+  # end
+
+  def create
+  end
+
+  # def show
+  # end
+  #
+  # def edit
+  # end
+  #
+  # def update
+  # end
 end
